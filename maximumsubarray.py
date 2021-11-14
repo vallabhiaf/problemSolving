4874c370d5480a2a9ae28f3e46f6533b062966ca
Vallabh Chugh <vallabh.iaf123@gmail.com>
2021-11-15 02:02:40 +0530

    dynamic problems

---
+++ maximumsubarray.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        finalSum=runningSum=nums[0]
+        
+        for i in nums[1:]:
+            runningSum=max(i,runningSum+i)
+            finalSum=max(finalSum,runningSum)
+            
+        return finalSum
