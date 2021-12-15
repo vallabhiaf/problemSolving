e40f07af8212e53ba5b87543607c804bf20dc4aa
Vallabh Chugh <vallabh.iaf123@gmail.com>
2021-12-15 17:21:31 +0530

    array

---
+++ PeeakElement.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+          
+            
+                
+            right=len(nums)-1
+            left=0
+            #need to check about right vs right-1
+            while left < right-1:
+                mid=(left+right)//2
+                if nums[mid] > nums[mid+1] and nums[mid] > nums[mid-1]:
+                    return mid
+                
+                if nums[mid] < nums[mid+1]:
+                    left=mid+1
+                else:
+                    right= mid-1
+                    
+            return left if nums[left] >= nums[right] else right
