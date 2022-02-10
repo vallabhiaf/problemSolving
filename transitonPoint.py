3b5f9c26c77cefb6b8389bb312b91bcea83ea9d8
Vallabh Chugh <vallabh.iaf123@gmail.com>
2022-02-10 12:46:11 +0530

    Question of the day

--- transitonPoint.py
+++ transitonPoint.py
@@ -1,13 +1,23 @@
 def transitionPoint(arr, n):
     #Code here
+     #Code here
     
     if arr[0] == 1:
         return 0
     if arr[n-1] == 0:
         return -1
-    for i in range(0,n):
-        if arr[i] == 1:
-            return i
+    low=0
+    high=n-1
+    ans=-1
+    while high >= low:
+        mid = (low+high)//2
+        if arr[mid] == 1:
+            ans = mid
+            high=mid-1
+        else:
+            low = mid +1
+            
+    return ans
 
 #{ 
 #  Driver Code Starts
