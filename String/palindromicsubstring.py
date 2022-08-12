68c559b3ddab436ba8f0303c629c3667c9e5a8e6
Vallabh Chugh <vallabh.iaf123@gmail.com>
2022-08-12 22:14:51 +0530

    distinct palindromic substring

--- String/palindromicsubstring.py
+++ String/palindromicsubstring.py
@@ -1,41 +1,24 @@
 class Solution:
-    def countdistinctSubstrings(s):
+    def countSubstrings(self, s: str) -> int:
         
         res = 0
-        res1=[]
 
         #Plan for odd and even subscting bring palindrome by taking each character as middle element and expaniding both ways. 
         
         for i in range(len(s)):
             l=r=i
             while l>=0 and r < len(s) and s[l]== s[r]:
-                sub=s[l:r+1]
-                if sub not in res1:
-                    res +=1
-                    res1.append(sub)
                 l -=1
                 r +=1
-                
-
-
-
+                res +=1
+            
             l=i
             r=i+1
             while l>=0 and r < len(s) and s[l]== s[r]:
-                sub=s[l:r+1]
-                if sub not in res1:
-                    res +=1
-                    res1.append(sub)
                 l -=1
                 r +=1
-                
+                res +=1
                 
         return res
-
-
-    
-    res=countdistinctSubstrings("aaa")
-    print(res)
-
             
                 
